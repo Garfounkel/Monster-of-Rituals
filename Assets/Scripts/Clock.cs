e5d12993dea2e94bd5bf51352e47e67c0ef46eeb
Garfounkel <andersen.simon.c@gmail.com>
2016-01-29 19:34:29 +0100

    Initial commit

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clock : MonoBehaviour {
+
+	public Transform clockNeedle;
+
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+		clockNeedle.RotateAround (transform.position, Vector3.right, Time.deltaTime * 5);
+	}
+}
