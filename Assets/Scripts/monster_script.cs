2ab8a6087e82d6efb5470cc26f9436ea40f7b4a3
Andreas Naustdal <and.epost@gmail.com>
2016-01-30 21:51:10 +0100

    Monsters running more smoothly and bounce off the player, making him angry

--- Assets/Scripts/monster_script.cs
+++ Assets/Scripts/monster_script.cs
@@ -1,34 +1,34 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 
 public class monster_script : MonoBehaviour {
 
     public bool _moving = true;
-    public float _movespeed = 0.05f;
-    Vector2 _movedirection;
+    public bool _movingRight = true;
+    public float Acceleration = 5;
+    public float MaxVelocity = 5;
+    public float BounceForce = 5;
     private Rigidbody2D _localrigid;
     AudioSource _audio_hardhit;
 
     // Use this for initialization
     void Start () {
         _audio_hardhit = GetComponent<AudioSource>();
-        _localrigid = GetComponent<Rigidbody2D>();   
-	}
+        _localrigid = GetComponent<Rigidbody2D>();
+        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerAngry>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
 
-        if (_moving == true)
+        if (_moving)
         {
-            _movedirection = new Vector2(
-
-                transform.position.x + _movespeed,
-                transform.position.y
-
-                );
-            _localrigid.MovePosition(_movedirection);
+            if (_localrigid.velocity.x < MaxVelocity) _localrigid.AddForce(new Vector2((_movingRight ? 1 : -1) * Acceleration, 0));
         }
+        
+        if (transform.position.y < -8 || Mathf.Abs(transform.position.x) > 12) Destroy(gameObject);
 
 	}
    /* void OnTriggerEnter2D(Collider2D _other)
@@ -45,20 +45,25 @@ public class monster_script : MonoBehaviour {
 
 
     }
-    void OnCollisionEnter2D(Collision2D _coll)
+
+    private void OnCollisionEnter2D(Collision2D _coll)
     {
-        Debug.Log("CollisionEnter");
-       if (_coll.gameObject.tag == "Tentacle")
-        
+        //Debug.Log("CollisionEnter");
+        if (_coll.gameObject.tag == "Tentacle")
+
             _moving = false;
-        
+
         if (_coll.relativeVelocity.magnitude > 0.1)
         {
-            _audio_hardhit.Play();
-            Debug.Log("Magnitude");
+            //_audio_hardhit.Play();
+            //Debug.Log("Magnitude");
         }
-           
-            
+
+        if (_coll.gameObject.tag == "Player")
+        {
+            PlayerAngry.MoreAngry();
+            _localrigid.AddForce(new Vector2((_movingRight ? -1 : 1), 1) * BounceForce,ForceMode2D.Impulse);
+        }
 
     }
 
--- Assets/Scripts/monster_spawner_script.cs
+++ Assets/Scripts/monster_spawner_script.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 public class monster_spawner_script : MonoBehaviour {
 
+    public int MaximumMonsters = 5;
+
     int _spawntimer = 0;
     Vector2 _spawnerposition;
+    private int _monsterCount = 0;
 
     public GameObject _monsterprefab;
     
@@ -21,17 +25,19 @@ public class monster_spawner_script : MonoBehaviour {
         _spawnerposition = new Vector2(
             transform.position.x,transform.position.y
             );
-      
 
-        
-        if (_spawntimer >= 20)
+
+
+        //if (_spawntimer >= 20 && _monsterCount < MaximumMonsters)
+        if (_spawntimer >= 20 && GameObject.FindGameObjectsWithTag("Monster").Count() < MaximumMonsters) //Is this slow?
         {
             GameObject _Monster = Instantiate(_monsterprefab);
             _Monster.name = "Monster";
             _Monster.transform.position = Random.insideUnitCircle * 2 + _spawnerposition;
 
             _spawntimer = 0;
-            Debug.Log("Spawn");
+            //Debug.Log("Spawn nr " + GameObject.FindGameObjectsWithTag("Monster").Count());
+            //_monsterCount++;
         }
 
 
