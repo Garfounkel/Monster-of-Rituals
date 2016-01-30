ae00240e9cff830a31673799279330a75e2ab63f
Andreas Naustdal <and.epost@gmail.com>
2016-01-30 17:03:10 +0100

    Added shower sound and tweakable jitteryness for when the shower is on

--- Assets/Scripts/Shower.cs
+++ Assets/Scripts/Shower.cs
@@ -12,6 +12,12 @@ public class Shower : MonoBehaviour
     private ParticleSystem _ps;
     private bool _emitting = true;
     private Transform _player;
+    private AudioSource _showerSound;
+    private Transform _mount;
+    public float _soundStart = 0.15f;
+    public float _soundLoop = 5;
+    public float _soundEnd = 5.5f;
+    public float JitterRate = 10;
 
     void Start ()
 	{
@@ -19,19 +25,29 @@ public class Shower : MonoBehaviour
         _ttc = GetComponent<TentacleTipController>();
         _ps = GetComponentInChildren<ParticleSystem>();
         _player = GameObject.Find("Player").transform;
-	}
+        _mount = transform.root.Find("Mount");
+        _showerSound = GetComponent<AudioSource>();
+        //_showerSound.Play();
+    }
 
     private void Update()
     {
         if (On)
         {
             _rb.AddRelativeForce(Force);
-            _ttc.enabled = false;
+            if (Random.Range(0, JitterRate) > 1)
+                _ttc.enabled = false;
+            else
+                _ttc.enabled = true;
             if (!_emitting)
             {
                 _ps.Play();
+                _showerSound.Play();
+                _showerSound.time = 0.15f;
+                //_showerSound.mute = false;
                 _emitting = true;
             }
+            else if (_showerSound.time > _soundLoop) _showerSound.time = _soundStart;
         }
         else
         {
@@ -41,11 +57,13 @@ public class Shower : MonoBehaviour
             if (_emitting)
             {
                 _ps.Stop();
+                //_showerSound.mute = true;
+                _showerSound.time = _soundEnd;
                 _emitting = false;
             }
         }
         //if (_player && _player.transform.position.x > transform.position.x-1) On = true;
-        if (_player && Mathf.Abs(_player.transform.position.x - transform.position.x) < TriggerDistance)
+        if (_player && Mathf.Abs(_player.transform.position.x - _mount.position.x) < TriggerDistance)
             On = true;
         else
             On = false;
