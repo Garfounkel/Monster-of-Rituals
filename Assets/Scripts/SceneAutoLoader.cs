d908855a0aff0e7e53d9ef89ef7b3ec436f924e9
Thomas <battlesheep5@gmail.com>
2016-01-30 20:24:07 +0100

    Scene autoloader gogoggo

---
+++ Assets/Scripts/SceneAutoLoader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneAutoLoader : MonoBehaviour {
+
+	public string[] otherScenes;
+
+	void Awake(){
+		for (int i = 0; i < otherScenes.Length; i++){
+			if (SceneManager.GetActiveScene().name != otherScenes[i]){
+				SceneManager.LoadScene(otherScenes[i], LoadSceneMode.Additive);
+			}
+		}
+	}
+}
