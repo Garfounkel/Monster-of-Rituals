1ec9e150a16d7427712ec69df0336fb6bd107e3d
Thomas <battlesheep5@gmail.com>
2016-01-31 13:11:15 +0100

    Made bakground scene

---
+++ Assets/Scripts/SkyController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyController : MonoBehaviour {
+
+	public SpriteRenderer skyRenderer;
+	public Color morningColor;
+	public Color midDayColor;
+	public Color eveningColor;
+	public Color nightColor;
+
+	void Update(){
+		if (Clock.instance != null){
+//			if (Clock.CurrentHour
+			skyRenderer.color = Color.blue * (Clock.CurrentHour / 24f); 
+//			Debug.Log("COLOR IS " + skyRenderer.color);
+		}
+	}
+
+}
