e276e9497604dba1d3d9604287b7a51dec854dad
Garfounkel <andersen.simon.c@gmail.com>
2016-01-31 12:26:26 +0100

    Bed interraction almost working

--- Assets/Scripts/BedInteraction.cs
+++ Assets/Scripts/BedInteraction.cs
@@ -1,14 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class BedInteraction : MonoBehaviour
 {
 
+    public FadeScreen fadeScreen;
+
+#if UNITY_EDITOR
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(Restart());
+        }
+    }
+#endif
+
     private void OnTriggerStay2D(Collider2D other)
     {
-        if (Clock.instance.wiskyDrinked && Clock.CurrentHour > 20 && other.GetComponent<ControlableBody>() != null)
+        if (Clock.instance.whiskyDrinked && Clock.CurrentHour > 20 && other.GetComponent<ControlableBody>() != null)
         {
-            Debug.Log("it's working");
+            StartCoroutine(Restart());
         }
+    }
+
+    private IEnumerator Restart()
+    {
+        Debug.Log("Restarting");
+        yield return StartCoroutine(fadeScreen.FadeOut());
+        SceneManager.LoadScene(0);
     }
 }
