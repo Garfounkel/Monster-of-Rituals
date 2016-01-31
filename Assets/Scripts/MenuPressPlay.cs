c85edbe75dd11ae9354611d6eb4315133bb9e9b9
Garfounkel <andersen.simon.c@gmail.com>
2016-01-31 16:15:09 +0100

    Menu stuff

---
+++ Assets/Scripts/MenuPressPlay.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPressPlay : MonoBehaviour {
+	void Update ()
+	{
+	    if (Input.GetKeyDown(KeyCode.Return))
+	    {
+	        SceneManager.LoadScene(1);
+	    }
+	}
+}
